c17f34ed88c6e2ff992d7d7dff9aed60d7d4be7f
Aleksa <aleksa.gavrilovic@guidefitter.com>
2021-11-22 11:59:02 +0100

    added message deduplication argument to delay queue

--- Transport/Connection.php
+++ Transport/Connection.php
@@ -407,6 +407,8 @@ class Connection
             // after being released from to DLX, make sure the original routing key will be used
             // we must use an empty string instead of null for the argument to be picked up
             'x-dead-letter-routing-key' => $routingKey ?? '',
+            // custom argument for message deduplication
+            'x-message-deduplication' => true
         ]);
 
         return $queue;
